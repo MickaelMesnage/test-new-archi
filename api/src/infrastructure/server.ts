124f11fd46962bb0363a6e00fd37a5158add2e6b
Mickael <mickael.mesnage@lonestone.io>
2025-04-30 23:44:41 +0200

    remove node module

--- api/src/infrastructure/server.ts
+++ api/src/infrastructure/server.ts
@@ -7,7 +7,7 @@ const server = createHTTPServer({
   middleware: cors(),
 });
 
-const port = Number(process.env.PORT) || 3000;
+const port = 3000;
 server.listen(port);
 
 console.log(`🚀 Server listening on port ${port}`);
